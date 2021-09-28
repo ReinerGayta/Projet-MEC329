d53ffb7853e5aab14cd205b4f101ac6786012f7f
alimawa <ali.mawassi.1@ens.etsmtl.ca>
2021-09-28 18:05:48 -0400

    Create liste_variables.m

---
+++ liste_variables.m
@@ -0,0 +1,25 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%LISTE DES VARIABLES
+%DATE:28 septembre 2021
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+L1="Longueur du bras inferieur";
+a1="Distance entre la goupille A et B";
+b1="Distance entre la goupille C et D";
+c1="Distance entre la goupille C et le centre du bras inferieur";
+d1="Distance entre la goupille D et le centre du bras inferieur";
+L2="Longueur du bras superieur";
+a2="Distance entre la goupille D et E";
+c2="Distance entre la goupille E et le centre du bras superieur";
+d2="Distance entre la goupille D et le centre du bras superieur";
+tetaMax="Angle incinaison entre les deux bras";
+hMax="Hauteur maximale de la grue";
+hM="Hauteur cible de la charge";
+W="Charge de 6000 Newtons";
+teta1="Inclinaison maximale du bras inferieur par rapport au sol";
+teta2="Inclinaison maximale du  bras superieur par rapport au sol";
+OB="Longueur du verin inferieur";
+CE="Longueur du verin superieur";
+alpha1="Angle entre le verin OB et l'axe horizontal";
+alpha2="Angle entre le verin CE et l'axe horizontal";
+Fs="Facteur de securite";
