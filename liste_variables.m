e3a6a20c51cb40087a07447379f09699f2c74201
ReinerGayta <91152180+ReinerGayta@users.noreply.github.com>
2021-10-05 17:47:19 -0400

    Update liste_variables.m

--- liste_variables.m
+++ liste_variables.m
@@ -3,23 +3,23 @@
 %DATE:28 septembre 2021
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-L1="Longueur du bras inferieur";
-a1="Distance entre la goupille A et B";
-b1="Distance entre la goupille C et D";
-c1="Distance entre la goupille C et le centre du bras inferieur";
-d1="Distance entre la goupille D et le centre du bras inferieur";
-L2="Longueur du bras superieur";
-a2="Distance entre la goupille D et E";
-c2="Distance entre la goupille E et le centre du bras superieur";
-d2="Distance entre la goupille D et le centre du bras superieur";
-tetaMax="Angle incinaison entre les deux bras";
-hMax="Hauteur maximale de la grue";
-hM="Hauteur cible de la charge";
-W="Charge de 6000 Newtons";
-teta1="Inclinaison maximale du bras inferieur par rapport au sol";
-teta2="Inclinaison maximale du  bras superieur par rapport au sol";
-OB="Longueur du verin inferieur";
-CE="Longueur du verin superieur";
-alpha1="Angle entre le verin OB et l'axe horizontal";
-alpha2="Angle entre le verin CE et l'axe horizontal";
-Fs="Facteur de securite";
+L1=4.1;     %"Longueur du bras inferieur";
+a1=0;     %"Distance entre la goupille A et B";
+b1=0;     %"Distance entre la goupille C et D";
+c1=0;     %"Distance entre la goupille C et le centre du bras inferieur";
+d1=0.3;     %"Distance entre la goupille D et le centre du bras inferieur";
+L2=4.1;    %"Longueur du bras superieur";
+a2=0;     %"Distance entre la goupille D et E";
+c2=0;     %"Distance entre la goupille E et le centre du bras superieur";
+d2=0.3;     %"Distance entre la goupille D et le centre du bras superieur";
+tetaMax=75;        %"Angle incinaison entre les deux bras";
+hMax=0;       %"Hauteur maximale de la grue";
+hM=0;     %"Hauteur cible de la charge";
+W=0;      %"Charge de 6000 Newtons";
+teta1=0;     %"Inclinaison maximale du bras inferieur par rapport au sol";
+teta2=0;     %"Inclinaison maximale du  bras superieur par rapport au sol";
+OB=0;     %"Longueur du verin inferieur";
+CE=0;    %"Longueur du verin superieur";
+alpha1=0;    %"Angle entre le verin OB et l'axe horizontal";
+alpha2=0;     %"Angle entre le verin CE et l'axe horizontal";
+Fs=0;     %"Facteur de securite";
