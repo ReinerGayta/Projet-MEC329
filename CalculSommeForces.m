d1bfac8738df16f449419dab9ecb4bd317565697
ReinerGayta <91152180+ReinerGayta@users.noreply.github.com>
2021-10-27 19:53:39 -0400

    Changer formules et ajoute des donnes

--- CalculSommeForces.m
+++ CalculSommeForces.m
@@ -10,14 +10,36 @@ vA2 = [xE-xD,yE-yD,0];
 
 vB1 = [xD-xC,yD-yC,0];
 
-sFABC = vFA + vFOB + vWABC + -vFCE + vWCE/2 + -vFD + vWOB/2 == 0;
 
-sFDEF = vFD + vFCE + vWDEF + vWCE/2 + vW == 0;
+sFABC = vFA + vFOB + vWABC + -vFCE + vWCE/2 + -vFD + vWOB/2;
 
-sMDDEF = cross(vL2,vW) + cross(vL2/2,vWDEF) + cross(vA2,vFCE) + cross(vA2,vWCE/2)==0; 
+sFDEF = vFD + vFCE + vWDEF + vWCE/2 + vW;
 
-sMAABC = cross(vL1,-vFD) + cross(vL1/2,vWABC) + cross(vL1-vB1,-vFCE) + cross(vA1,vFOB) + cross(vL1-vB1,vWCE/2) + cross(vA1,vWOB/2)==0; 
+sMDDEF = cross(vL2,vW) + cross(vL2/2,vWDEF) + cross(vA2,vFCE) + cross(vA2,vWCE/2); 
 
-[fCE,fOB,fDx,fDy,fAx,fAy] = solve(sFABC,sFDEF,sMDDEF,sMAABC,[fCE,fOB,fDx,fDy,fAx,fAy]);
+sMAABC = cross(vL1,-vFD) + cross(vL1/2,vWABC) + cross(vL1-vB1,-vFCE) + cross(vA1,vFOB) + cross(vL1-vB1,vWCE/2) + cross(vA1,vWOB/2); 
 
+
+sommeForcesDEFX = sFDEF(1) == 0;
+sommeForcesDEFY = sFDEF(2) == 0;
+sommeMomentsDDEF = sMAABC(3) == 0;
+
+sommeForcesABCX  = sFABC(1) == 0;
+sommeForcesABCY = sFABC(2) == 0;
+sommeMomentsAABC = sMAABC(3) ==0;
+
+[solForcesCE,solForcesOB,solForcesDx,solForcesDy,solForcesAx,solForcesAy] = solve(sommeForcesABCX,sommeForcesABCY,sommeForcesDEFX, sommeForcesDEFY,sommeMomentsAABC,sommeMomentsDDEF,[fCE,fOB,fDx,fDy,fAx,fAy]);
+
+vFA
+fprintf('%f \n', solForcesAx);
+
+fprintf('%f \n', solForcesAy);
+
+fprintf('%f \n', solForcesDx);
+
+fprintf('%f\n', solForcesDy);
+
+fprintf('%f\n', solForcesCE);
+
+fprintf('%f\n', solForcesOB);
 
--- liste_variables.m
+++ liste_variables.m
@@ -4,14 +4,14 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %Longueurs
-L1=4.1;     %"Longueur du bras inferieur";
-a1=0;     %"Distance entre la goupille A et B";
-b1=0;     %"Distance entre la goupille C et D";
-c1=0;     %"Distance entre la goupille C et le centre du bras inferieur";
-d1=0.3;     %"Distance entre la goupille D et le centre du bras inferieur";
-L2=4.1;    %"Longueur du bras superieur";
-a2=0;     %"Distance entre la goupille D et E";
-c2=0;     %"Distance entre la goupille E et le centre du bras superieur";
+L1=5;     %"Longueur du bras inferieur";
+a1=1;     %"Distance entre la goupille A et B";
+b1=1;     %"Distance entre la goupille C et D";
+c1=.2;     %"Distance entre la goupille C et le centre du bras inferieur";
+d1=0.1;     %"Distance entre la goupille D et le centre du bras inferieur";
+L2=5;    %"Longueur du bras superieur";
+a2=2;     %"Distance entre la goupille D et E";
+c2=.1;     %"Distance entre la goupille E et le centre du bras superieur";
 d2=0.3;     %"Distance entre la goupille D et le centre du bras superieur";
 tetaMax=75;        %"Angle incinaison entre les deux bras";
 xO = 0.4;
@@ -19,13 +19,13 @@ yO = -0.4;
 teta = 0;
 %Forces
 
-fW = 8000;
+fW = 8;
 
 aireSection = input("Entrez l'aire de la section de votre poutre \n");
 
-fWABC = L1 * aireSection * 7850 * 9.81 ; 
+fWABC = 100; 
 
-fWDEF = L2 * aireSection * 7850 * 9.81 ; 
+fWDEF = 100 ; 
 
 fWCE = input("Entrez le poids d'un des verins \n");
 
