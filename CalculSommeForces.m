a674446c1ed5c698406fd75d099c69c56576e010
ReinerGayta <91152180+ReinerGayta@users.noreply.github.com>
2021-10-27 17:19:07 -0400

    Changed 2d matrices to 3d matrices

--- CalculSommeForces.m
+++ CalculSommeForces.m
@@ -1,11 +1,23 @@
 %Nom : SystèmesDéquations
 
-sFABC = vFA + vFOB + vWABC + vFCE + vWCE/2 + vFD + vWOB/2 == 0;
+vL1 = [xD,yD,0];
+
+vA1 = [xB,yB,0];
+
+vL2 = [xF-xD,yF-yD,0];
+
+vA2 = [xE-xD,yE-yD,0];
+
+vB1 = [xD-xC,yD-yC,0];
+
+sFABC = vFA + vFOB + vWABC + -vFCE + vWCE/2 + -vFD + vWOB/2 == 0;
 
 sFDEF = vFD + vFCE + vWDEF + vWCE/2 + vW == 0;
 
-sMDDEF = cross(L2,vW) + cross(L2/2,vWDEF) + cross(a2,vFCE) + cross(a2,vWCE/2)==0; 
+sMDDEF = cross(vL2,vW) + cross(vL2/2,vWDEF) + cross(vA2,vFCE) + cross(vA2,vWCE/2)==0; 
 
-sMAABC = cross([xD,yD,0],vFD) + cross(L1/2,vWABC) + cross(L1-b1,vFCE) + cross(a1,vFOB) + cross(L1-b1,vWCE/2) + cross(a1,vWOB/2)==0; 
+sMAABC = cross(vL1,-vFD) + cross(vL1/2,vWABC) + cross(vL1-vB1,-vFCE) + cross(vA1,vFOB) + cross(vL1-vB1,vWCE/2) + cross(vA1,vWOB/2)==0; 
 
-[fCE,fOB,fDx,fDy,dAx,fAy] = solve(sFABC,sFDEF,sMDDEF,sMAABC,[fCE,fOB,fDx,fDy,dAx,fAy]);
+[fCE,fOB,fDx,fDy,fAx,fAy] = solve(sFABC,sFDEF,sMDDEF,sMAABC,[fCE,fOB,fDx,fDy,fAx,fAy]);
+
+
--- ExpressionDesForces.m
+++ ExpressionDesForces.m
@@ -2,22 +2,22 @@
 
 syms fCE fOB fDx fDy fAx fAy;
 
-vCE = [xE-xC,yE-yC];
+vCE = [xE-xC,yE-yC,0];
 vFCE = fCE * vCE / norm(vCE);
 
-vOB = [xB-xO,yB-yO];
+vOB = [xB-xO,yB-yO,0];
 vFOB = fOB * vOB / norm(vOB);
 
-vFD = [fDx,fDy];
+vFD = [fDx,fDy,0];
 
-vFA = [fAx,fAy];
+vFA = [fAx,fAy,0];
 
-vW = [0,fW];
+vW = [0,fW,0];
 
-vWABC = [0,fWABC];
+vWABC = [0,fWABC,0];
 
-vWDEF = [0,fWDEF];
+vWDEF = [0,fWDEF,0];
 
-vWCE = [0,fWCE];
+vWCE = [0,fWCE,0];
 
-vWOB = [0,fWOB];
+vWOB = [0,fWOB,0];
