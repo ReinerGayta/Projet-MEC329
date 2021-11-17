573ea19960622e7184f2c15b4c6f516d28e54f5a
ReinerGayta <91152180+ReinerGayta@users.noreply.github.com>
2021-11-17 13:19:40 -0500

    Changement des valeurs

--- liste_variables.m
+++ liste_variables.m
@@ -6,7 +6,7 @@
 %Longueurs
 L1=4.1;     %"Longueur du bras inferieur";
 a1=1;     %"Distance entre la goupille A et B";
-b1=1.1;     %"Distance entre la goupille C et D";
+b1=.6;     %"Distance entre la goupille C et D";
 c1=.2;     %"Distance entre la goupille C et le centre du bras inferieur";
 d1=0.1;     %"Distance entre la goupille D et le centre du bras inferieur";
 L2=4.1;    %"Longueur du bras superieur";
@@ -14,7 +14,7 @@ a2=2;     %"Distance entre la goupille D et E";
 c2=.1;     %"Distance entre la goupille E et le centre du bras superieur";
 d2=0.3;     %"Distance entre la goupille D et le centre du bras superieur";
 tetaMax=75;        %"Angle incinaison entre les deux bras";
-xO = 0.4;
+xO = 0.2;
 yO = -0.4;
 teta = 0;
 teta1 = input("Entrez l'angle d'inclinaison du bras ABC \n");
